2636855065fdf54c05ca6242403605fbb5a48cbc
Alejandro Lopez <alope131@asu.edu>
2021-10-19 22:25:45 -0700

    updates in MovieApiClient

--- app/src/main/java/com/lexandroid/movieapp/request/MovieApiClient.java
+++ app/src/main/java/com/lexandroid/movieapp/request/MovieApiClient.java
@@ -12,6 +12,7 @@ import com.lexandroid.movieapp.response.MovieSearchResponse;
 import com.lexandroid.movieapp.utils.Credentials;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -34,7 +35,7 @@ public class MovieApiClient {
     private RetrieveMovieRunnable retrieveMovieRunnable;
 
     //Making Global RUNNABLE for popular
-    private RetrieveMovieRunnable retrievePopularMovieRunnable;
+    private RetrievePopularMovieRunnable retrievePopularMovieRunnable;
 
 
 
@@ -52,6 +53,10 @@ public class MovieApiClient {
 
     public LiveData<MovieModel> getMovies() {
         return mMovies;
+    }
+
+    public LiveData<List<MovieModel>> getPopularMovies() {
+        return mMoviesPopular;
     }
 
 
@@ -73,6 +78,26 @@ public class MovieApiClient {
                 myHandler.cancel(true);
             }
         }, 3000, TimeUnit.MILLISECONDS);
+    }
+
+    //1 This method we are calling for popular movies
+    public void searchPopularMoviesApi(int page) {
+        Log.d("Debug", "inside searchPopularMoviesApi inside MovieApiClient");
+        if(retrievePopularMovieRunnable != null) {
+            retrievePopularMovieRunnable = null;
+        }
+
+        retrievePopularMovieRunnable = new RetrievePopularMovieRunnable(page);
+        Log.d("Debug", "Made it past RetrieveMovieRunnable inside MovieApiClient");
+        final Future myHandlerPopular = AppExecutors.getInstance().networkIO().submit(retrievePopularMovieRunnable);
+
+        AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                //Cancelling the retrofit call
+                myHandlerPopular.cancel(true);
+            }
+        }, 1000, TimeUnit.MILLISECONDS);
     }
 
     //Retrieving data from rest api
@@ -121,6 +146,75 @@ public class MovieApiClient {
                         query,
                         page
                 );
+        }
+
+        private Call<MovieSearchResponse> getPopularMovies(int page) {
+            return Service.getTmdbApi().getPopularMovies(
+                    Credentials.API_KEY,
+                    page
+            );
+        }
+
+        private Call<MovieModel> getSpecificMovie(int movieId) {
+            return Service.getTmdbApi().getSpecificMovie(
+                    movieId,
+                    Credentials.API_KEY
+            );
+        }
+
+        private void cancelRequest() {
+            Log.v("Tag", "Cancelling Search Request");
+            cancelRequest = true;
+        }
+    }
+
+    private class RetrievePopularMovieRunnable implements Runnable {
+
+        private int page;
+        boolean cancelRequest;
+
+        public RetrievePopularMovieRunnable(int page) {
+            this.page = page;
+            cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+            //Getting the response objects
+            try {
+                Response response = getPopularMovies(page).execute();
+                if (cancelRequest) {
+                    return;
+                }
+                if(response.code() == 200) {
+                    List<MovieModel> list = new ArrayList<>(((MovieSearchResponse)response.body()).getMovies());
+                    if(page == 1) {
+                        mMoviesPopular.postValue(list);
+                    }else {
+                        List<MovieModel> currentMovies = mMoviesPopular.getValue();
+                        currentMovies.addAll(list);
+                        mMoviesPopular.postValue(currentMovies);
+                    }
+
+                }else {
+                    String error = response.errorBody().string();
+                    Log.v("Tag", "Error: " + error);
+                    mMoviesPopular.postValue(null);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                mMoviesPopular.postValue(null);
+            }
+
+        }
+
+        //Search method/query
+        private Call<MovieSearchResponse> getMovies(String query, int page) {
+            return Service.getTmdbApi().searchForMovies(
+                    Credentials.API_KEY,
+                    query,
+                    page
+            );
         }
 
         private Call<MovieSearchResponse> getPopularMovies(int page) {

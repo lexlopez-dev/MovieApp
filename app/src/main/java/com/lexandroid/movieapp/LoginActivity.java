92adac75557f93f37e4ca5060aed86c6fe581d21
lexlopez-dev <a.lopezcr9@gmail.com>
2021-11-11 20:55:02 -0700

    Updates to login screen

---
+++ app/src/main/java/com/lexandroid/movieapp/LoginActivity.java
@@ -0,0 +1,90 @@
+package com.lexandroid.movieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.L;
+import com.airbnb.lottie.LottieAnimationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+import com.lexandroid.movieapp.login.LoginAdapter;
+
+public class LoginActivity extends AppCompatActivity {
+
+    //Animation
+    ImageView logo, splashBg, tmdb;
+    TextView pwrdBy;
+    LottieAnimationView lottieAnimationView;
+
+    //Login & Signup
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FloatingActionButton google, google2, google3;
+    float v = 0;
+
+    //Buttons
+    AppCompatButton buttonGuest;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introductory);
+
+        //logo = findViewById(R.id.logo_splash_screen);
+        splashBg = findViewById(R.id.imgSplash);
+        tmdb = findViewById(R.id.logo_tmdb);
+        pwrdBy = findViewById(R.id.pwrdBy);
+
+        buttonGuest = findViewById(R.id.guestButton);
+
+       // lottieAnimationView = findViewById(R.id.lottieAnimationMovie);
+
+        splashBg.animate().translationY(-3600).setDuration(500).setStartDelay(3000);
+        //logo.animate().translationY(2400).setDuration(1000).setStartDelay(2000);
+        pwrdBy.animate().translationY(2400).setDuration(500).setStartDelay(3000);
+        tmdb.animate().translationY(2400).setDuration(500).setStartDelay(3000);
+        //lottieAnimationView.animate().translationY(2400).setDuration(1000).setStartDelay(4000);
+
+        //Login & Signup
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        google = findViewById(R.id.fab_google);
+        google2 = findViewById(R.id.fab_google2);
+        google3 = findViewById(R.id.fab_google3);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Login"));
+        tabLayout.addTab(tabLayout.newTab().setText("Signup"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(), this, tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+
+
+        google.setTranslationY(300);
+        google2.setTranslationY(300);
+        google3.setTranslationY(300);
+        tabLayout.setTranslationY(300);
+
+        google.setAlpha(v);
+        google2.setAlpha(v);
+        google3.setAlpha(v);
+        tabLayout.setAlpha(v);
+
+        google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(3400).start();
+        google2.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(3600).start();
+        google3.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(3800).start();
+
+        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(3100).start();
+
+
+    }
+}
---
+++ app/src/main/java/com/lexandroid/movieapp/login/LoginTabFragment.java
@@ -0,0 +1,21 @@
+package com.lexandroid.movieapp.login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.lexandroid.movieapp.R;
+
+public class LoginTabFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.login_tab_fragment, container, false);
+
+        return root;
+    }
+
+}
